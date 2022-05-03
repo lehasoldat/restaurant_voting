7e47ba3db57ba98142c84071f9d2e7df05068bbc
lehasoldat <saldakeev@gmail.com>
2022-05-03 09:35:43 +0300

    init

---
+++ src/test/java/com/github/lehasoldat/restaurant_voting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lehasoldat.restaurant_voting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
